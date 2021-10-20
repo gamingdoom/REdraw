8fc222ad4333ca69a0a09f241c043e566db6c8a7
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2021-10-19 21:32:19 -0700

    might work idk

--- copyandcontinue.js
+++ copyandcontinue.js
@@ -1,15 +1,29 @@
 var canvas = document.getElementById('canvas');
 
+function getWants()
+{
+    var dlcheck = document.getElementById('dl');
+    if (dlcheck.checked == true)
+    {
+        copyandcontinue();
+    }
+    else
+    {
+        window.location.href = "describe.html";
+    }
+}
+
 function copyandcontinue()
 {
 // create temporary link  
 var imageData = canvas.toDataURL("image/png", 0.7);
 var tmpLink = document.createElement( 'a' );  
-tmpLink.download = 'canvas.png'; // set the name of the download file 
+tmpLink.download = 'image.png'; // set the name of the download file 
 tmpLink.href = imageData;  
   
 // temporarily add link to body and initiate the download  
 document.body.appendChild( tmpLink );  
 tmpLink.click();  
 document.body.removeChild( tmpLink );
+window.location.href = "describe.html";
 }
---
+++ getlink.js
@@ -0,0 +1,29 @@
+var input = document.getElementById('description').value.replace(/\ /g,"+");
+var linkout = "https://gamingdoom.github.io/REdraw/draw2.html?desc=" + document.getElementById('description').value.replace(/\ /g,"+");
+console.log(input);
+var alinkout = document.getElementById('linktxt');
+var tarea = document.getElementById('description');
+var sendlink = document.getElementById('sendlink');
+var linck = document.getElementById('linck');
+
+sendlink.style.visibility = 'hidden';
+linck.style.visibility = 'hidden';
+tarea.addEventListener('keypress', getlink);
+
+
+function getlink()
+{
+    console.log(document.getElementById('description').value);
+    linkout = "https://gamingdoom.github.io/REdraw/draw2.html?desc=" + document.getElementById('description').value.replace(/\ /g,"+");
+    console.log(linkout);
+    alinkout.textContent = linkout;
+    sendlink.style.visibility = 'visible';
+    linck.style.visibility = 'visible';
+}
+
+function copylink()
+{
+    linkout = "https://gamingdoom.github.io/REdraw/draw2.html?desc=" + document.getElementById('description').value.replace(/\ /g,"+");
+    alinkout.textContent = linkout;
+    navigator.clipboard.writeText(linkout);
+}

bed46bb189c7172b30d870399226ca8a96697e3d
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2021-10-25 21:56:52 -0700

    testing on prod bc im smart

--- copyandcontinue.js
+++ copyandcontinue.js
@@ -5,14 +5,32 @@ function getWants(out)
     var dlcheck = document.getElementById('dl');
     if (dlcheck.checked == true)
     {
+        upimg();
         copyandcontinue();
     }
     else
     {
+        upimg();
         window.location.href = out;
     }
 }
-
+function upimg()
+{
+    var imageData = canvas.toDataURL("image/png")
+    $.ajax({
+        type: "POST",
+        url: "upimg.php",
+        data: { 
+           imgBase64: imageData
+        }
+      }).done(function(o) {
+        console.log('saved'); 
+        // If you want the file to be visible in the browser 
+        // - please modify the callback in javascript. All you
+        // need is to return the url to the file, you just saved 
+        // and than put the image in your browser.
+      });
+}
 function copyandcontinue(out)
 {
 // create temporary link  

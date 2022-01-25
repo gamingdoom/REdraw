e812a9dd1793630fa3074ac9e986785c8c9bd39f
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2022-01-25 15:45:34 -0800

    Remove useless broken code and add play again button

--- copyandcontinue.js
+++ copyandcontinue.js
@@ -3,7 +3,7 @@ var canvas = document.getElementById('canvas');
 function getWants(out)
 {
     var dlcheck = document.getElementById('dl');
-    if (dlcheck.checked == true)
+    if (dlcheck.checked)
     {
         //upimg();
         copyandcontinue(out);
@@ -33,15 +33,15 @@ function getWants(out)
 // }
 function copyandcontinue(out)
 {
-// create temporary link  
-var imageData = canvas.toDataURL("image/png", 0.7);
-var tmpLink = document.createElement( 'a' );  
-tmpLink.download = 'image.png'; // set the name of the download file 
-tmpLink.href = imageData;  
-  
-// temporarily add link to body and initiate the download  
-document.body.appendChild( tmpLink );  
-tmpLink.click();  
-document.body.removeChild( tmpLink );
-window.location.href = out;
+    // create temporary link  
+    var imageData = canvas.toDataURL("image/png", 0.7);
+    var tmpLink = document.createElement( 'a' );  
+    tmpLink.download = 'image.png'; // set the name of the download file 
+    tmpLink.href = imageData;  
+    
+    // temporarily add link to body and initiate the download  
+    document.body.appendChild( tmpLink );  
+    tmpLink.click();  
+    document.body.removeChild( tmpLink );
+    window.location.href = out;
 }
